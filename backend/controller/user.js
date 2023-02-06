95f86ced93059b417eef1bd428151993bd1ecf8a
zzsealy <1147575371@qq.com>
2023-02-06 22:24:00 +0800

    fix: 浏览器没有收到返回值

--- backend/controller/user.js
+++ backend/controller/user.js
@@ -8,21 +8,21 @@ usersRouter.post('login', (request, response) => {
 })
 
 
-usersRouter.post('/register', (request, response) => {
+usersRouter.post('/register', async (request, response) => {
 
     const { username, password, passwordRepeat } = request.body;
     // const findData = 
     const findUser = await userModel.find({ 'username': username })
     if (findUser.length) {
         console.log('存在')
-        response.status(200).json({'message': '已存在'})
+        return response.status(200).json({'message': '已存在'})
     } else {
         const user = new userModel({
             username,
             password
         })
         const saveUser = user.save()
-        response.status(201).json(saveUser)
+        return response.status(200).json(saveUser)
     }
     
 })
