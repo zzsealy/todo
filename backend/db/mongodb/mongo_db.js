ce82f1cf9e3ebfe523ad7224a1c443cbb9460197
zzsealy <1147575371@qq.com>
2023-02-27 15:07:30 +0800

    feat: docker-compose 完成

--- backend/db/mongodb/mongo_db.js
+++ backend/db/mongodb/mongo_db.js
@@ -2,7 +2,16 @@
 
 const mongoose = require('mongoose')
 const config = require('../../utils/config')
-const URL = config.MONGODB_URI
+const {
+    DB_HOST,
+    MONGODB_PASSWORD,
+    MONGODB_USER,
+    DB_PORT,
+    MONGODB_DATABASE,
+} = process.env;
+const URL = `mongodb://${MONGODB_USER}:${MONGODB_PASSWORD}@${DB_HOST}:${DB_PORT}/${MONGODB_DATABASE}`
+// mongodb://todo_admin:drq12345!@127.0.0.1:27017/todo
+console.log('=======', URL)
 
 mongoose.connect(URL);
 
@@ -18,7 +27,8 @@ db.on('error', function(error) {
     mongoose.disconnect();
 });
 
-db.on('close', function() {
+db.on('close', function () {
+    console.log(URL)
     console.error('数据库断开，重新连接数据库');
     mongoose.connect(URL, {server:{auto_reconnect:true}});
 });
