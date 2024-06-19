6de6f4b01f667451ef1f9ea5d0a2d4c3bfa1c100
dairuiquan <1147575371@qq.com>
2024-06-19 17:37:38 +0800

    feat: 迁移到next.js 初始化

---
+++ front-next/app/layout.tsx
@@ -0,0 +1,17 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={`${inter.className} antialiased`}>{children}</body>
+    </html>
+  );
+}
